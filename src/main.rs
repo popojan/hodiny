28c56eb97ecf2b61b71c2692a6bcff5d04928e61
Jan Popelka <popojan@protonmail.com>
2023-05-20 15:27:50 +0200

    in memory midi poc

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,118 @@
 
 use std::fs::File;
 use std::sync::Arc;
-use rustysynth::{SoundFont, Synthesizer, SynthesizerSettings};
-use rodio::{OutputStream, Sink, source::Source};
-use rodio::buffer::SamplesBuffer;
-
-fn get_waveform(synthesizer: &mut Synthesizer, channel: i32, note: i32, sample_count: usize, velocity: i32) -> Vec<f32> {
-    synthesizer.note_on(channel, note, velocity); //E4 44 B4 51
-
-    let mut left: Vec<f32> = vec![0_f32; sample_count];
-    let mut right: Vec<f32> = vec![0_f32; sample_count];
-
-    synthesizer.render(&mut left[..], &mut right[..]);
-
-    left
-        .into_iter()
-        .zip(right.into_iter())
-        .flat_map(|(l,r)| vec![l, r])
-        .collect::<Vec<f32>>()
-}
+use rustysynth::{MidiFile, MidiFileSequencer, SoundFont, Synthesizer, SynthesizerSettings};
+use tinyaudio::{OutputDeviceParameters, run_output_device};
+use itertools::Itertools;
+use midly::{Format, Header, Smf, Timing, Track, TrackEvent, TrackEventKind};
+use midly::MetaMessage::{EndOfTrack, Tempo};
+use midly::MidiMessage::{NoteOff, NoteOn, ProgramChange};
 
 fn main() {
+
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    let mut left: Vec<f32> = vec![0_f32; params.channel_sample_count];
+    let mut right: Vec<f32> = vec![0_f32; params.channel_sample_count];
 
     let mut sf2 = File::open("jeux14.sf2").unwrap();
     let sound_font = Arc::new(SoundFont::new(&mut sf2).unwrap());
 
-    let settings = SynthesizerSettings::new(44100);
-    let mut synthesizer = Synthesizer::new(&sound_font, &settings).unwrap();
+    let mut new_midi = Smf::new(Header { format: Format::SingleTrack, timing: Timing::Metrical(128.into()) });
+    let mut track = Track::new();
+    track.push(TrackEvent {
+        delta: 0.into(),
+        kind: TrackEventKind::Meta(
+            Tempo(
+                1000_000.into())),
+    });
+    track.push(TrackEvent {
+       delta: 0.into(),
+       kind: TrackEventKind::Midi {
+           channel: 1.into(),
+           message: ProgramChange {
+               program:
+               123.into(),
+           }
+       }
+    });
+    track.push(TrackEvent {
+        delta: 0.into(),
+        kind: TrackEventKind::Midi {
+            channel: 1.into(),
+            message: NoteOn {
+                key: 58.into(),
+                vel: 96.into(),
+            }
+        }
+    });
+    track.push(TrackEvent {
+        delta: 128.into(),
+        kind: TrackEventKind::Midi {
+            channel: 1.into(),
+            message: NoteOn {
+                key: 51.into(),
+                vel: 96.into(),
+            }
+        }
+    });
+    track.push(TrackEvent {
+        delta: 256.into(),
+        kind: TrackEventKind::Midi {
+            channel: 1.into(),
+            message: NoteOff {
+                key: 58.into(),
+                vel: 96.into(),
+            }
+        }
+    });
+    track.push(TrackEvent {
+        delta: 256.into(),
+        kind: TrackEventKind::Midi {
+            channel: 1.into(),
+            message: NoteOff {
+                key: 51.into(),
+                vel: 96.into(),
+            }
+        }
+    });
 
-    synthesizer.process_midi_message(1, 0xB0, 0x00, 0); // choose bank
-    synthesizer.process_midi_message(1, 0xC0, 123, 0); // choose patch
+    track.push(TrackEvent {
+        delta: 0.into(),
+        kind: TrackEventKind::Meta(EndOfTrack),
+    });
+    new_midi.tracks.push(track);
+    let mut in_memory = Vec::new();
+    new_midi.write(&mut in_memory).unwrap();
 
 
-    let sample_count = (2.5 * settings.sample_rate as f32).round() as usize;
-    let quarter_samples = get_waveform(&mut synthesizer, 1, 58, sample_count, 96);
-    let hour_samples = get_waveform(&mut synthesizer, 1, 51, sample_count, 96);
+    //eprintln!("in_memory.len() {}", in_memory.len());
 
-    let quarter = || SamplesBuffer::new(2, settings.sample_rate as u32, quarter_samples.clone());
-    let hour = || SamplesBuffer::new(2, settings.sample_rate as u32, hour_samples.clone());
+    let mid = MidiFile::new(&mut in_memory.as_slice()).unwrap();
 
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let midi_file = Arc::new(mid);
 
+    let settings = SynthesizerSettings::new(params.sample_rate as i32);
+    let synthesizer = Synthesizer::new(&sound_font, &settings).unwrap();
 
-    let sink = Sink::try_new(&stream_handle).unwrap();
-    sink.append(quarter().convert_samples::<f32>());
-    sink.append(quarter().convert_samples::<f32>());
-    sink.append(hour().convert_samples::<f32>());
-    sink.append(hour().convert_samples::<f32>());
+    let mut sequencer = MidiFileSequencer::new(synthesizer);
 
-    sink.sleep_until_end();
+    sequencer.play(&midi_file, false);
+
+    let _device = run_output_device(params, {
+        move |data| {
+            sequencer.render(&mut left[..], &mut right[..]);
+            for (i, value) in left.iter().interleave(right.iter()).enumerate() {
+                data[i] = *value;
+            }
+        }
+    })
+    .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(10));
+
 }
