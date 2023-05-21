3131fd3eed053c0d2568485ed807e9cad642cc73
Jan Popelka <popojan@protonmail.com>
2023-05-21 16:44:28 +0200

    westminster quarters

--- src/main.rs
+++ src/main.rs
@@ -30,43 +30,21 @@ fn note(track: &mut Track, key: u7, vel: u7, delta: u28, on_not_off: bool) -> u6
     delta.as_int().into()
 }
 
-fn main() {
-
-    let config_text = fs::read_to_string("hodiny.toml").unwrap();
-    let config: Config = toml::from_str(&config_text).unwrap();
-
-    let params = OutputDeviceParameters {
-        channels_count: 2,
-        sample_rate: 44100,
-        channel_sample_count: 4410,
-    };
+fn simple_or_full(mut track: &mut Track, config: &Config) -> u64 {
     let now = Local::now();
-
     let now_hour = now.hour() % 12;
     let now_minute = (now.minute() as f64 / 15.0).floor() as u32;
 
     let num_hour_strikes = if now_hour == 0 {12} else {now_hour};
     let num_quarter_strikes = if now_minute == 0 {4} else {now_minute};
 
-    let mut left: Vec<f32> = vec![0_f32; params.channel_sample_count];
-    let mut right: Vec<f32> = vec![0_f32; params.channel_sample_count];
-
-    let mut sf2 = File::open(config.striking.soundfont).unwrap();
-    let sound_font = Arc::new(SoundFont::new(&mut sf2).unwrap());
-
-    let mut new_midi = Smf::new(Header {
-        format: Format::SingleTrack,
-        timing: Timing::Metrical(
-            config.tempo.ticks_per_beat.into())
-    });
-
-    let mut track = Track::new();
     track.push(TrackEvent {
         delta: 0.into(),
         kind: TrackEventKind::Meta(
             Tempo(
                 config.tempo.microseconds_per_beat.into())),
     });
+
     track.push(TrackEvent {
         delta: 0.into(),
         kind: TrackEventKind::Midi {
@@ -81,13 +59,16 @@ fn main() {
     let quarter_note = config.quarter.note.into();
     let quarter_velocity = config.quarter.velocity.into();
     let quarter_ticks = config.quarter.delta.into();
+    let quarters_hours_rest_ticks = config.striking.rest.into();
+
     for strike in 0 .. num_quarter_strikes {
-        let delta_ticks = if strike == 0 {
-            u28::new(0)
+        let delta_ticks = if strike == num_quarter_strikes - 1 {
+            quarter_ticks + quarters_hours_rest_ticks
         } else {
             quarter_ticks
         };
-        duration += note(&mut track, quarter_note, quarter_velocity, delta_ticks, true);
+        duration += note(&mut track, quarter_note, quarter_velocity, u28::new(0), true);
+        duration += note(&mut track, quarter_note, quarter_velocity, delta_ticks, false);
     }
 
     track.push(TrackEvent {
@@ -102,30 +83,147 @@ fn main() {
     let hour_ticks = config.hour.delta.into();
     let hour_note = config.hour.note.into();
     let hour_velocity = config.hour.velocity.into();
-    let quarters_hours_rest_ticks = config.striking.rest.into();
 
     if config.striking.kind > 0 || num_quarter_strikes == 4 {
         for strike in 0..num_hour_strikes {
-            let delta_ticks = if strike == 0 {
-                quarter_ticks + quarters_hours_rest_ticks
+            let delta_ticks = if strike == num_hour_strikes - 1 {
+                hour_ticks + quarters_hours_rest_ticks
             } else {
                 hour_ticks
             };
-            duration += note(&mut track, hour_note, hour_velocity, delta_ticks, true);
+            duration += note(&mut track, hour_note, hour_velocity, u28::new(0), true);
+            duration += note(&mut track, hour_note, hour_velocity, delta_ticks, false);
         }
     }
-    duration += note(&mut track, quarter_note, quarter_velocity, hour_ticks, false);
-    duration += note(&mut track, hour_note, hour_velocity, hour_ticks, false);
+    let extra = hour_ticks.as_int() as u64 * 4;
+    duration += extra;
+
+    track.push(TrackEvent {
+        delta: u28::new(extra as u32),
+        kind: TrackEventKind::Meta(EndOfTrack),
+    });
+
+    return duration;
+}
+
+fn westminster(mut track: &mut Track, config: &Config) -> u64 {
+    let now = Local::now();
+    let now_hour = now.hour() % 12;
+    let now_minute = (now.minute() as f64 / 15.0).floor() as u32;
+
+    let num_hour_strikes = if now_hour == 0 {12} else {now_hour};
+    let num_quarter_strikes = if now_minute == 0 {4} else {now_minute};
 
     track.push(TrackEvent {
         delta: 0.into(),
+        kind: TrackEventKind::Meta(
+            Tempo(
+                config.tempo.microseconds_per_beat.into())),
+    });
+
+    track.push(TrackEvent {
+        delta: 0.into(),
+        kind: TrackEventKind::Midi {
+            channel: 1.into(),
+            message: ProgramChange {
+                program: config.quarter.program.into(),
+            }
+        }
+    });
+    /*  https://en.wikipedia.org/wiki/Westminster_Quarters
+            G♯4, F♯4, E4, B3	68 66 64 59
+            E4, G♯4, F♯4, B3	64 68 66 59
+            E4, F♯4, G♯4, E4	64 66 68 64
+            G♯4, E4, F♯4, B3	68 64 66 59
+            B3, F♯4, G♯4, E4	59 66 68 64
+    */
+    let changes = vec![
+        vec![68, 66, 64, 59],
+        vec![64, 68, 66, 59],
+        vec![64, 66, 68, 64],
+        vec![68, 64, 66, 59],
+        vec![59, 66, 68, 64],
+    ];
+    let quarter_changes = vec![
+        vec![1], vec![2, 3], vec![4, 5, 1], vec![2, 3, 4, 5]
+    ];
+    let mut duration: u64 = 0;
+    let quarter_velocity = config.quarter.velocity.into();
+    let quarter_ticks: u32 = config.quarter.delta;
+    let quarters_hours_rest_ticks: u32 = config.striking.rest.into();
+
+    for (i_change, &change) in quarter_changes[num_quarter_strikes as usize-1].iter().enumerate() {
+        for (i_note, &n) in changes[change-1].iter().enumerate() {
+            let delta_ticks = if i_note == 3 {
+                if i_change == quarter_changes[num_quarter_strikes as usize-1].len() - 1 {
+                    (quarter_ticks * 2 + quarters_hours_rest_ticks as u32).into()
+                } else {
+                    (quarter_ticks * 2).into()
+                }
+            } else {
+                quarter_ticks.into()
+            };
+            duration += note(&mut track, n.into(), quarter_velocity, u28::new(0), true);
+            duration += note(&mut track, n.into(), quarter_velocity, delta_ticks, false);
+        }
+    }
+    let hour_ticks: u32 = config.hour.delta.into();
+    if num_quarter_strikes == 4 || config.striking.kind > 2 {
+        let hour_note = config.hour.note.into();
+        let hour_velocity = config.hour.velocity.into();
+
+        for strike in 0..num_hour_strikes {
+            let delta_ticks = if strike == num_hour_strikes - 1 {
+                hour_ticks + quarters_hours_rest_ticks
+            } else {
+                hour_ticks
+            }.into();
+            duration += note(&mut track, hour_note, hour_velocity, u28::new(0), true);
+            duration += note(&mut track, hour_note, hour_velocity, delta_ticks, false);
+        }
+    }
+
+    let extra = hour_ticks as u64 * 4;
+    duration += extra;
+
+    track.push(TrackEvent {
+        delta: u28::new(extra as u32),
         kind: TrackEventKind::Meta(EndOfTrack),
     });
+    return duration;
+}
+
+fn main() {
+
+    let config_text = fs::read_to_string("hodiny.toml").unwrap();
+    let config: Config = toml::from_str(&config_text).unwrap();
+
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    let mut sf2 = File::open(&config.striking.soundfont).unwrap();
+    let sound_font = Arc::new(SoundFont::new(&mut sf2).unwrap());
+
+    let mut new_midi = Smf::new(Header {
+        format: Format::SingleTrack,
+        timing: Timing::Metrical(
+            config.tempo.ticks_per_beat.into())
+    });
+
+    let mut track = Track::new();
+
+    let duration = if config.striking.kind < 2 {
+        simple_or_full(&mut track, &config)
+    } else {
+        westminster(&mut track, &config)
+    };
 
     new_midi.tracks.push(track);
     let mut in_memory = Vec::new();
     new_midi.write(&mut in_memory).unwrap();
-
 
     let mid = MidiFile::new(&mut in_memory.as_slice()).unwrap();
 
@@ -137,6 +235,9 @@ fn main() {
     let mut sequencer = MidiFileSequencer::new(synthesizer);
 
     sequencer.play(&midi_file, false);
+
+    let mut left: Vec<f32> = vec![0_f32; params.channel_sample_count];
+    let mut right: Vec<f32> = vec![0_f32; params.channel_sample_count];
 
     let _device = run_output_device(params, {
         move |data| {
